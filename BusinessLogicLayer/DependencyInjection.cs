e9b82b6a366112e640b1c78c2360741754fdc0d7
mashrief <msshrief@gmail.com>
2026-04-30 12:06:48 +0600

    Implemented Layered Architecture With Exception Handling Middleware

---
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLogicLayer
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
--- ProductsMicroService.API/Program.cs
+++ ProductsMicroService.API/Program.cs
@@ -1,6 +1,19 @@
+using BusinessLogicLayer;
+using DataAccessLayer;
+using ProductsMicroService.API.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDataAccessLayer();
+builder.Services.AddBusinessLogicLayer();
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseExceptionHandlingMiddleware();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
