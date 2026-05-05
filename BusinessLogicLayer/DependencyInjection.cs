b42be353dee54d7c278ad1cfe2ccbe2087656fa0
mashrief <msshrief@gmail.com>
2026-05-05 07:29:45 +0600

    Implemented Swagger And Cors

--- BusinessLogicLayer/DependencyInjection.cs
+++ BusinessLogicLayer/DependencyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLogicLayer.Mappers;
 using BusinessLogicLayer.ServiceContracts;
 using BusinessLogicLayer.Services;
+using BusinessLogicLayer.Validators;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BusinessLogicLayer
@@ -10,6 +12,7 @@ namespace BusinessLogicLayer
         public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services)
         {
             services.AddAutoMapper(typeof(ProductAddRequestToProductMappingProfile).Assembly);
+            services.AddValidatorsFromAssemblyContaining<ProductAddRequestValidator>();
             services.AddTransient<IProductsService, ProductsService>();
 
             return services;
--- ProductsMicroService.API/Program.cs
+++ ProductsMicroService.API/Program.cs
@@ -3,18 +3,38 @@ using DataAccessLayer;
 using FluentValidation.AspNetCore;
 using ProductsMicroService.API.APIEndpoints;
 using ProductsMicroService.API.Middlewares;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataAccessLayer(builder.Configuration);
 builder.Services.AddBusinessLogicLayer();
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
 builder.Services.AddControllers();
 builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("http://localhost:4200")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 app.UseExceptionHandlingMiddleware();
 app.UseRouting();
+app.UseCors();
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
